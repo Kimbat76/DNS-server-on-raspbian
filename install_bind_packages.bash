d158ef3edcad533dde43d6c949228769f5bcacf2
Kimbat76 <37043102+Kimbat76@users.noreply.github.com>
2018-03-21 22:14:04 +0100

    v1.1
    adding update command for refreshing the apt-get database
    adding y option to assume yes to all

--- install_bind_packages.bash
+++ install_bind_packages.bash
@@ -6,8 +6,11 @@
 # 2. bind9utils
 # 3. dnsutils
 
+# to update the apt-get database
+sudo apt-get update
+
 # request to install bind packages
-sudo apt-get install bind9 bind9utils dnsutils
+sudo apt-get -y install bind9 bind9utils dnsutils
 
 # adding the bind9 daemon to the rc defaults level
 # on this way, the dns server will be started automatically at each startup
