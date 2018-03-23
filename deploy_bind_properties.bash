d739e0e75771b93243a3c7a647c38e99559501b5
Kimbat76 <37043102+Kimbat76@users.noreply.github.com>
2018-03-23 22:07:43 +0100

    v1.6
    adding sudo for copying the bind_properties

--- deploy_bind_properties.bash
+++ deploy_bind_properties.bash
@@ -6,7 +6,7 @@ source_dir="bind_properties"
 # list the properties files to deploy
 for list_file in $(find ${source_dir} -type f)
 do
-	cp -p ${list_file} ${target_dir}/
+	sudo cp -p ${list_file} ${target_dir}/
 done
 
 #restart bind9 deamon for activating the effects.
